6414e12f1af06faeae83faf67b0e972b1bd2b0df
KatsiarynaBukraba <36957276+KatsiarynaBukraba@users.noreply.github.com>
2018-03-02 13:40:41 +0300

    Add program's files

---
+++ DuplicateSymbols.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LineAnalysis
+{
+    /// <summary>
+    ///  This class is about search dublicate symbols in a string.
+    /// </summary>
+    class DuplicateSymbols
+    {
+        private string userString;
+
+        public DuplicateSymbols(string args)
+        {
+            userString = args;
+        }
+
+        /// <summary>
+        /// This method researches users string to found maximal series of duplicate symbols.
+        /// </summary>
+        public int MaxSerialDuplicateSymbols()
+        {
+            int count = 0;
+            int maxCount = 0;
+
+            for(int i = 0; i < userString.Length - 1; i++)
+            {
+                if (userString[i].Equals(userString[i+1]))
+                {
+                    count++;
+                }
+                else
+                {
+                    if(count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                    count = 0;
+                }
+
+            }
+
+            return maxCount + 1;
+            
+        }
+    }
+}
---
+++ EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LineAnalysis
+{
+    /// <summary>
+    /// It's an entry point in the program.
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// It's an entry point in the program.
+        /// </summary>
+        /// <param name="args">It's the users string to input.</param>
+        static void Main(string[] args)
+        {
+            DuplicateSymbols userString = new DuplicateSymbols(args[0]);
+            Console.WriteLine(userString.MaxSerialDuplicateSymbols());
+        }
+    }
+}
