4d17ca5422f33970da66b581042de380ac4b0761
KatsiarynaBukraba <36957276+KatsiarynaBukraba@users.noreply.github.com>
2018-03-02 23:04:38 +0300

    correct mistakes

---
+++ LineAnalysis/DuplicateSymbols.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LineAnalysis
+{
+    /// <summary>
+    ///  This class contains methods for work with duplicate symbols.
+    /// </summary>
+    class DuplicateSymbols
+    {
+        private string UserString { get; set; }
+
+        public DuplicateSymbols(string args)
+        {
+            UserString = args;
+        }
+
+        /// <summary>
+        /// This method calculates maximal series of duplicate symbols of user's string.
+        /// </summary>
+        /// <returns>maximal length of the series of duplicate symbols</returns>
+        public int MaxSerialDuplicateSymbols()
+        {
+            int currentCount = 1;
+            int maxCount = 1;
+
+            for (int i = 0; i < UserString.Length - 1; i++)
+            {
+                if (UserString[i].Equals(UserString[i + 1]))
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    if (currentCount > maxCount)
+                    {
+                        maxCount = currentCount;
+                    }
+                    currentCount = 0;
+                }
+            }
+            return maxCount;
+        }
+    }
+}
---
+++ LineAnalysis/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LineAnalysis
+{
+    /// <summary>
+    /// It's an entry point in the program.
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// It's an entry point in the program.
+        /// </summary>
+        /// <param name="args">the user's string to input</param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                DuplicateSymbols inputString = new DuplicateSymbols(args[0]);
+                Console.WriteLine(inputString.MaxSerialDuplicateSymbols());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+            }
+        }
+    }
+}
