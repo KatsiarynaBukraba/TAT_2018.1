7f9552579e22b08c14fe73f4ea9c2fc16a069f5e
KatsiarynaBukraba <36957276+KatsiarynaBukraba@users.noreply.github.com>
2018-03-02 12:30:25 +0300

    add new file

---
+++ Hello.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hello
+{
+    /// <summary>
+    /// This class is about to display some message.
+    /// </summary>
+    class Hello
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Some message");
+        }
+    }
+}
