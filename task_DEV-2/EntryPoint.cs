0c75f078886e897e1a8b3a900e9087d1b22f469e
KatsiarynaBukraba <36957276+KatsiarynaBukraba@users.noreply.github.com>
2018-03-04 12:52:39 +0300

    add new file

---
+++ task_DEV-2/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace task_DEV_2
+{
+    /// <summary>
+    /// The entry point to the program.
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// The entry point to the program.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                SequenceEvenIndex inputString = new SequenceEvenIndex(args[0]);
+                inputString.FindSequenceEvenIndex();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+            }
+        }
+    }
+}
---
+++ task_DEV-2/SequenceEvenIndex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace task_DEV_2
+{
+    /// <summary>
+    /// This class finds sequence of even indices.
+    /// </summary>
+    class SequenceEvenIndex
+    {
+        private string InputString { get; set; }
+
+        public SequenceEvenIndex(string value)
+        {
+            InputString = value;
+        }
+
+        /// <summary>
+        /// This method outputs sequence of even indices.
+        /// </summary>
+        public void FindSequenceEvenIndex()
+        {
+            StringBuilder sequenceEvenIndex = new StringBuilder(InputString);
+
+            for(int i = 1; i < sequenceEvenIndex.Length - 1; i++)
+            {
+                sequenceEvenIndex.Remove(i, 1);
+            }
+
+            Console.WriteLine(sequenceEvenIndex);
+        }
+    }
+}
