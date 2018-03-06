0d799c333c9715e95d5d94482b2a3907d8689520
KatsiarynaBukraba <36957276+KatsiarynaBukraba@users.noreply.github.com>
2018-03-06 22:18:40 +0300

    Update ConverterToNewSystem.cs

--- task_DEV-3/ConverterToNewSystem.cs
+++ task_DEV-3/ConverterToNewSystem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace task_DEV_3
@@ -25,12 +25,12 @@ namespace task_DEV_3
 
             do
             {
-                convertNumber.Append(GetDigit(inputNumber % baseOfNewSystem));
+                convertNumber.Insert(0, GetDigit(inputNumber % baseOfNewSystem));
                 inputNumber /= baseOfNewSystem;
             }
             while (inputNumber > 0);
 
-           return ReverseValue(convertNumber);
+           return convertNumber.ToString();
         }
 
         private char GetDigit(int numberOfDigit)
@@ -38,18 +38,6 @@ namespace task_DEV_3
             string allDigits = "0123456789ABCDEFGHIJ";
 
             return allDigits[numberOfDigit];
-        }
-
-        private string ReverseValue(StringBuilder value)
-        {
-            StringBuilder reverseValue = new StringBuilder();
-
-            for(int i = 0; i < value.Length; i++)
-            {
-                reverseValue.Append(value[value.Length-1-i]);
-            }
-
-            return reverseValue.ToString();
         }
     }
 }
