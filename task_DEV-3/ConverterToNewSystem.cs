1d2d2570ce30a35f4129477812487b2a2314103a
KatsiarynaBukraba <36957276+KatsiarynaBukraba@users.noreply.github.com>
2018-03-06 22:09:26 +0300

    add files

---
+++ task_DEV-3/ConverterToNewSystem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace task_DEV_3
+{
+    /// <summary>
+    /// This class converts the number in other numeral system.
+    /// </summary>
+    class ConverterToNewSystem
+    {
+        private int baseOfNewSystem;
+
+        public ConverterToNewSystem(int valueBase)
+        {
+            baseOfNewSystem = valueBase;
+        }
+
+        /// <summary>
+        /// This method converts the one number to the other in numeral system with input base.
+        /// </summary>
+        /// <returns>the number to convert</returns>
+        public string GetConvertNumber(int inputNumber)
+        {
+            StringBuilder convertNumber = new StringBuilder();
+
+            do
+            {
+                convertNumber.Append(GetDigit(inputNumber % baseOfNewSystem));
+                inputNumber /= baseOfNewSystem;
+            }
+            while (inputNumber > 0);
+
+           return ReverseValue(convertNumber);
+        }
+
+        private char GetDigit(int numberOfDigit)
+        {
+            string allDigits = "0123456789ABCDEFGHIJ";
+
+            return allDigits[numberOfDigit];
+        }
+
+        private string ReverseValue(StringBuilder value)
+        {
+            StringBuilder reverseValue = new StringBuilder();
+
+            for(int i = 0; i < value.Length; i++)
+            {
+                reverseValue.Append(value[value.Length-1-i]);
+            }
+
+            return reverseValue.ToString();
+        }
+    }
+}
---
+++ task_DEV-3/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace task_DEV_3
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+
+            int inputNumber; 
+            int baseOfNewSystem;
+
+            bool checkNumber = Int32.TryParse(args[0], out inputNumber);
+            bool checkFormatBase = Int32.TryParse(args[1], out baseOfNewSystem);
+            bool checkIntervalBase = baseOfNewSystem > 2 && baseOfNewSystem < 20;
+
+            if (checkNumber && checkFormatBase && checkIntervalBase)
+            {
+                ConverterToNewSystem converter = new ConverterToNewSystem(baseOfNewSystem);
+                Console.WriteLine(converter.GetConvertNumber(inputNumber));
+            }
+            else
+            {
+                Console.WriteLine("Error! Incorrect format of the data!");
+            }
+        }
+    }
+}
