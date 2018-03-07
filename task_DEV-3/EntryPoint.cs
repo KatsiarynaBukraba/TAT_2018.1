1365f97de9c1043c453c252dcadc9337d73e0bce
KatsiarynaBukraba <36957276+KatsiarynaBukraba@users.noreply.github.com>
2018-03-07 14:53:03 +0300

    update

--- task_DEV-3/EntryPoint.cs
+++ task_DEV-3/EntryPoint.cs
@@ -7,12 +7,6 @@ namespace task_DEV_3
     {
         static void Main(string[] args)
         {
-            if (!(args.Length == 2))
-            {
-                Console.WriteLine("Error! Incorrect format of the data!");
-                return;
-            }
-
             try
             {
                 long inputNumber = Int64.Parse(args[0]);
