9189d92cea21f937cffc2c062ca25cb27631c5eb
KatsiarynaBukraba <36957276+KatsiarynaBukraba@users.noreply.github.com>
2018-03-16 16:05:57 +0300

    refactoring

--- task_DEV-2/SequenceEvenIndexBuilder.cs
+++ task_DEV-2/SequenceEvenIndexBuilder.cs
@@ -6,21 +6,19 @@ namespace task_DEV_2
     /// <summary>
     /// This class finds the sequence of even indices.
     /// </summary>
-    class SequenceEvenIndexBuilder
+    public class SequenceEvenIndexBuilder
     {
-        private string inputString;
-
-        public SequenceEvenIndexBuilder(string value)
-        {
-            inputString = value;
-        }
-
         /// <summary>
         /// This method returns the sequence of even indices.
         /// </summary>
         /// <returns>the sequence of even indices</returns>
-        public string FindSequenceEvenIndex()
+        public string FindSequenceEvenIndex(string inputString)
         {
+            if (inputString.Equals(string.Empty))
+            {
+                throw new ArgumentNullException();
+            }
+
             StringBuilder sequenceEvenIndex = new StringBuilder(inputString);
 
             for(int i = 1; i < sequenceEvenIndex.Length; i++)
